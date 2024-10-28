8c79d30bcb426da4cd6e6fe0bcb79457d49cd08c
Raja Pandiyan <rajapandiyan1163@gmail.com>
2024-10-28 09:32:46 +0530

    front end url add origin

--- MemoBack/app.js
+++ MemoBack/app.js
@@ -4,13 +4,13 @@ const bodyparser=require("body-parser");
 const Router=require("./Router/handlers");
 const cors=require("cors");
 
-app.use(cors({credentials:true,origin:"http://localhost:3000"}))
+app.use(cors({credentials:true,origin:"https://memo-fb77qy0pz-rajapandiyan1s-projects.vercel.app"}))
 app.use(bodyparser({extended:false}));
 app.use(bodyparser.json());
 (async()=>{
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/Memo").then(()=>{
+mongoose.connect(`mongodb+srv://rajapandiyan1163:r1a2j3a4@cluster0.8edduvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Memo`).then(()=>{
     console.log("Database connected successfully !")
 }).catch((e)=>{
     console.log(e)
